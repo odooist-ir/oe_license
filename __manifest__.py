3f99670d3e2d0bab244441676f9844bef9697da6
Amir Rismani <amir.rismaani@gmail.com>
2025-06-07 11:26:46 +0330

    all version

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "My Odoo License(Odooers.ir)",
+    "category": "Localization/Iran",
+    "version": "0.1",
+    'author': "odooers ir",
+    'summary': 'Bypass Odoo License',
+    'description': """
+          We have to crack the Odoo and we have no other choice because we cannot buy it even if we have money. we are sorry ...
+    """,
+    "website": "https://www.odooers.ir",
+    "license": "AGPL-3",
+    "summary": "Iran License",
+    "depends": ["mail"],
+    "data": [
+        "data/config.xml",
+    ],
+
+    "installable": True,
+}
